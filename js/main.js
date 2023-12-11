a920ea577f7e2c1db68b9642e35a69d5e650843d
ChubarovOleksandr <122209410+ChubarovOleksandr@users.noreply.github.com>
2023-12-11 23:43:06 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,37 @@
+$(document).ready(function() {
+    $('.slider').slick({
+        arrows: false,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        speed: 1000,
+    });
+})
+$(document).ready(function() {
+    $('.review__slider').slick({
+        arrows: false,
+        autoplay: true,
+        autoplaySpeed: 6000,
+        speed: 1300,
+        dots: true,
+        initialSlide: 1,
+        touchThreshold: 11,
+    });
+})
+$(document).ready(function () {
+    $('.prom').slick({
+        dots: true,
+        arrows: false,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        speed: 1000,
+    });
+})
+$(document).ready(function () {
+    $('.pobut').slick({
+        dots: true,
+        arrows: false,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        speed: 1000,
+    });
+})
